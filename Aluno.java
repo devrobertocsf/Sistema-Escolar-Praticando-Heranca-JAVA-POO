262154bf131a18879314a466433b63bb7b32bbaf
devrobertocsf <robertocarlosw5251@gmail.com>
2026-05-13 10:50:51 -0300

    código

---
+++ Aluno.java
@@ -0,0 +1,30 @@
+package pacote10;
+
+public class Aluno extends Pessoa {
+
+private int matr;
+private String curso;
+
+// método
+
+public void cancelarMatr() {
+	
+}
+
+
+//métodos especiais 
+public int getMatr() {
+	return matr;
+}
+public void setMatr(int matr) {
+	this.matr = matr;
+}
+public String getCurso() {
+	return curso;
+}
+public void setCurso(String curso) {
+	this.curso = curso;
+}
+
+
+}
